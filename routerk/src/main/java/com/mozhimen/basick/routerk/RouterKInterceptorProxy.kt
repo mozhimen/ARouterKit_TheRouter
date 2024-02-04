ae8dc18b3600eaa8f11a0e78ccf5567c8daafbb9
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-02-04 18:39:36 +0800

    0204

--- routerk/src/main/java/com/mozhimen/basick/routerk/RouterKInterceptorProxy.kt
+++ routerk/src/main/java/com/mozhimen/basick/routerk/RouterKInterceptorProxy.kt
@@ -1,8 +1,8 @@
 package com.mozhimen.basick.routerk
 
 import com.mozhimen.basick.elemk.androidx.lifecycle.bases.BaseWakeBefDestroyLifecycleObserver
-import com.mozhimen.basick.lintk.optin.OptInApiCall_BindLifecycle
-import com.mozhimen.basick.lintk.optin.OptInApiInit_ByLazy
+import com.mozhimen.basick.lintk.optins.OApiCall_BindLifecycle
+import com.mozhimen.basick.lintk.optins.OApiInit_ByLazy
 import com.therouter.router.RouteItem
 import com.therouter.router.interceptor.RouterInterceptor
 
@@ -13,8 +13,8 @@ import com.therouter.router.interceptor.RouterInterceptor
  * @Date 2024/1/15
  * @Version 1.0
  */
-@OptInApiInit_ByLazy
-@OptInApiCall_BindLifecycle
+@OApiInit_ByLazy
+@OApiCall_BindLifecycle
 class RouterKInterceptorProxy : BaseWakeBefDestroyLifecycleObserver() {
     fun setRouterInterceptor(interceptor: RouterInterceptor) {
         com.therouter.router.setRouterInterceptor(interceptor)
