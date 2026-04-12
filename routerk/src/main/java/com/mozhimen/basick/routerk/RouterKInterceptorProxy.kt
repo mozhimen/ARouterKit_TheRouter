79a6b83a6620b986bf57be670d30e81610d04b60
Kolin Zhao & Mozhimen <mozhimen@163.com>
2026-04-12 22:46:38 +0800

    260412

--- routerk/src/main/java/com/mozhimen/basick/routerk/RouterKInterceptorProxy.kt
+++ routerk/src/main/java/com/mozhimen/basick/routerk/RouterKInterceptorProxy.kt
@@ -1,8 +1,9 @@
 package com.mozhimen.basick.routerk
 
 import com.mozhimen.basick.bases.BaseWakeBefDestroyLifecycleObserver
-import com.mozhimen.kotlin.lintk.optins.OApiCall_BindLifecycle
-import com.mozhimen.kotlin.lintk.optins.OApiInit_ByLazy
+import com.mozhimen.kotlin.lintk.optins.api.OApiCall_BindLifecycle
+import com.mozhimen.kotlin.lintk.optins.api.OApiCall_BindViewLifecycle
+import com.mozhimen.kotlin.lintk.optins.api.OApiInit_ByLazy
 import com.therouter.router.RouteItem
 import com.therouter.router.interceptor.RouterInterceptor
 
@@ -13,6 +14,7 @@ import com.therouter.router.interceptor.RouterInterceptor
  * @Date 2024/1/15
  * @Version 1.0
  */
+@OApiCall_BindViewLifecycle
 @OApiInit_ByLazy
 @OApiCall_BindLifecycle
 class RouterKInterceptorProxy : BaseWakeBefDestroyLifecycleObserver() {
